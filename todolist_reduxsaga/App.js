a972e8d8de3e148e4974b7813d19e835c5b8e8e0
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2024-10-31 10:02:00 +0700

    done action

---
+++ todolist_reduxsaga/App.js
@@ -0,0 +1,21 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  
+});
---
+++ todolist_reduxsaga/redux/actions.js
@@ -0,0 +1,31 @@
+export const FETCH_TODOS="FETCH_TODOS";
+export const FETCH_TODOS_SUCCESS = "FETCH_TODOS_SUCCES";
+export const FETCH_TODOS_FAIL = "FETCH_TODOS_FAIL";
+
+export const ADD_TODO = "ADD_TODO";
+export const ADD_TODO_SUCCESS = "ADD_TODO_SUCCESS";
+export const ADD_TODO_FAIL= "ADD_TODO_FAIL";
+
+export const UPDATE_TODO = "UPDATE_TODO";
+export const UPDATE_TODO_SUCCESS = "UPDATE_TODO_SUCCESS";
+export const UPDATE_TODO_FAIL= "UPDATE_TODO_FAIL";
+
+export const DELETE_TODO = "DELETE_TODO";
+export const DELETE_TODO_SUCCESS = "DELETE_TODO_SUCCESS";
+export const DELETE_TODO_FAIL= "ADD_TODO_FAIL";
+
+export const fetchTodos= ()=> ({
+  type: FETCH_TODOS
+});
+export const addTodo = (todo)=>({
+  type: ADD_TODO,
+  payload: todo
+});
+export const updateTodo = (todo)=>({
+  type: UPDATE_TODO,
+  payload: todo
+});
+export const deleteTodo = (id)=>({
+  type: DELETE_TODO,
+  payload: id
+})
